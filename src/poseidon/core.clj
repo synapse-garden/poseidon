07fe245386cbea3d0c1190e92e388b28a63f363b
Bodie Solomon <binary132@gmail.com>
2014-04-25 02:29:38 -0400

    Added encrypted credentials for testing and way too many functions!

--- src/poseidon/core.clj
+++ src/poseidon/core.clj
@@ -2,22 +2,67 @@
   [:require [clojure.data.json :as json]]
   [:require [clj-http.client :as client]])
 
-(def creds
-  {:api-key ""
-   :client-id ""})
+(def api-base "https://api.digitalocean.com/")
 
-(def debug-mode? true)
+(defn rekey
+  [mp ks]
+  (if (> (count ks) 0)
+    (rekey (get mp (first ks)) (rest ks))
+    mp))
 
-(def web-creds
-  (str "client_id=" (creds :client-id) "&api_key=" (creds :api-key)))
+(defn env
+  "The value of an environment variable k."
+  [k] (System/getenv k))
 
-(def base "https://api.digitalocean.com/")
+(defn creds
+  "DO credentials map based on environment variables $DO_CLIENT_ID and $DO_API_KEY."
+  []
+  {:api-key (env "DO_API_KEY")
+   :client-id (env "DO_CLIENT_ID")})
 
-(def digitalocean-paths {:droplets (str base "droplets?" web-creds)})
+(defn url-creds
+  "The credential login map."
+  ([] (str "client_id=" ((creds) :client-id) "&api_key=" ((creds) :api-key)))
+  ([c]
+     (if (and (c :client-id) (c :api-key))
+       (str "client_id=" (c :client-id) "&api_key=" (c :api-key))
+       nil)))
 
-(defn basic-query
-  "A basic query against DigitalOcean API."
+(defn digitalocean-queries
+  "Queries."
+  [querykeys]
+  (let [path (fn [route] (str api-base route (url-creds)))
+        args-ok? (fn [expected actual] (if (= (expected) (keys actual)) true false))
+        query-fn (fn [route expected] (fn [params]
+                                      (if (args-ok? expected params)
+                                        (str (path route)
+                                             (for [k expected]
+                                               (if (= k (first expected))
+                                                 (str (name k) "=" (get params k))
+                                                 (str "&" (name k) "=" (get params k))))))))
+        queries {:droplets
+                 {:get-all (query-fn "droplets?" nil)
+                  :new (query-fn "droplets/new?" [:name :size :image])}}]
+    (rekey queries querykeys)))
+     ;; :show (query-fn "droplets/")}))
+     ;;  :or }
+     ;; :regions () regions-query
+     ;; :images images-query
+     ;; :sshkeys sshkeys-query
+     ;; :sizes sizes-query
+     ;; :domains domains-query
+     ;; :events events-query}))
+
+(defn api-query
+  "A query against DigitalOcean API."
   [& {:keys [debug]
-      :or {debug debug-mode?}}]
+      :or {debug false}}]
   (json/read-str
-   ((client/get (digitalocean-paths :droplets) {:throw-exceptions (not debug)}) :body)))
+   ((client/get
+     ((digitalocean-queries [:droplets :get-all]))
+     {:throw-exceptions (not debug)}) :body)))
+
+;;(let [drops ((basic-query) "droplets")
+;;      ks (keys (first drops))]
+;;(for [drop drops
+;;      k ks] [k (get drop k)]))
